1e86977411bbd5cbd6be7975de946eaf6ce6d6ec
plusleung <42401487+plusleung@users.noreply.github.com>
2022-09-09 15:57:54 +0800

    Update normal.py

--- normal.py
+++ normal.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 for p in [(0,1),(0,2),(0,3)]:
     u, s = p
-    x = np.linspace(u-3*s, u+3*s, 100)
+    x = np.linspace(u-3*s, u+3*s, 101)
     plt.plot(x, 1 / (np.sqrt(2 * np.pi) * s) * np.exp(-(x-u)**2 / (2 * s**2)),
              label=r'$\mu=%.1f,\ \sigma=%.1f$' % (u, s))
 
