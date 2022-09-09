6ef09d7ee9699a47e07463d679a5538a78078e36
plusleung <42401487+plusleung@users.noreply.github.com>
2022-09-09 14:52:41 +0800

    Update gamma.py

--- gamma.py
+++ gamma.py
@@ -5,7 +5,7 @@ from math import factorial
 for p in [(1, 1), (5, 1), (10, 1), (10, 2), (10, 4)]:
     a, b = p[0], p[1]
     u, s = a/b, a**0.5/b
-    x = np.arange(0, 20, 0.1, dtype=float)
+    x = np.arange(0, 20, 0.1)
     plt.plot(x, b**a / factorial(a-1) * (x**(a-1)) * np.exp(-b*x), 
              label=r'$\alpha=%d,\ \beta=%.1f,\ \mu=%.1f,\ \sigma=%.1f$' % (a, b, u, s))
 
