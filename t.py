aa5bdef71ffbc37dd486f9cea9136d1c1d44236c
plusleung <42401487+plusleung@users.noreply.github.com>
2022-09-14 10:58:35 +0800

    Update t.py

--- t.py
+++ t.py
@@ -7,11 +7,11 @@ from math import gamma
 def t(freedom):
     x = np.linspace(-6, 6, 1001)
     y = gamma((freedom + 1) / 2) / np.sqrt(freedom * np.pi) / gamma(freedom / 2) * (1 + x**2 / freedom) ** (-((freedom + 1) / 2))
-    return x, y, np.mean(y), np.std(y)
+    return x, y, np.std(y)
 
 for freedom in [1, 2, 5]:
-    x, y, u, s = t(freedom)
-    plt.plot(x, y, label=r'$v=%d,\ \mu=%.3f,\ \sigma=%.3f$' % (freedom, u, s))
+    x, y, s = t(freedom)
+    plt.plot(x, y, label=r'$v=%d,\ \sigma=%.3f$' % (freedom, s))
 
 plt.legend()
 plt.savefig('/mnt/d/student_t.png')
