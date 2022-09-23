aa8de02d767ff4e99748cd1e4915dd38bc561948
plusleung <42401487+plusleung@users.noreply.github.com>
2022-09-23 17:14:09 +0800

    Update chi-squared.py

--- chi-squared.py
+++ chi-squared.py
@@ -4,12 +4,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import gamma
+from matplotlib import ticker
 
 for k in [1,2,3,4,6,11]:
     x = np.arange(0.5, 15, 0.1)
     plt.plot(x, 1 / 2**(k/2) / gamma(k/2) * x**(k/2 - 1) * np.exp(-x/2),
              label=r'$k=%d$' % (k))
 
+plt.gca().yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1,decimals=0))
+    
 plt.legend()
 plt.savefig('/mnt/d/chi-squared.png')
 plt.show()
