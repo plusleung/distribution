f72c52582f5ca7d3fb0ce0d0a546dcca6c76bb0e
plusleung <42401487+plusleung@users.noreply.github.com>
2022-09-23 17:13:25 +0800

    Update gamma.py

--- gamma.py
+++ gamma.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import factorial
+from matplotlib import ticker
 
 for p in [(1, 1), (5, 1), (10, 1), (10, 2), (10, 4)]:
     a, b = p
@@ -8,6 +9,8 @@ for p in [(1, 1), (5, 1), (10, 1), (10, 2), (10, 4)]:
     x = np.arange(0, 20, 0.1)
     plt.plot(x, b**a / factorial(a-1) * (x**(a-1)) * np.exp(-b*x), 
              label=r'$\alpha=%d,\ \beta=%.1f,\ \mu=%.1f,\ \sigma=%.1f$' % (a, b, u, s))
+
+plt.gca().yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1,decimals=0))
 
 plt.legend()
 plt.savefig('/mnt/d/gamma.png')
