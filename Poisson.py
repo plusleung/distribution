aef92ab3f4797e2bcc5291706690d10c8ac49504
plusleung <42401487+plusleung@users.noreply.github.com>
2022-09-23 17:18:29 +0800

    Update Poisson.py

--- Poisson.py
+++ Poisson.py
@@ -7,7 +7,7 @@ r=range(21)
 
 for p in [(3,'r'),(5,'g'),(7,'b')]:
     for k in r:
-        plt.scatter(k,p[0]**k * np.exp(-p[0]) / factorial(k),color=p[1])
+        plt.scatter(k,p[0]**k * np.exp(-p[0]) / factorial(k), color=p[1])
 
 plt.xticks(r[::1])
 plt.gca().yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1,decimals=0))
