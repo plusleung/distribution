9730600deebfc7dd9716c4ecd9ff1677eb5d6583
plusleung <42401487+plusleung@users.noreply.github.com>
2022-09-23 17:15:03 +0800

    Update Poisson.py

--- Poisson.py
+++ Poisson.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import factorial
+from matplotlib import ticker
 
 r=range(21)
 
@@ -9,5 +10,7 @@ for p in [(3,'r'),(5,'g'),(7,'b')]:
         plt.scatter(k,p[0]**k * np.exp(-p[0]) / factorial(k),color=p[1])
 
 plt.xticks(r[::1])
+plt.gca().yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1,decimals=0))
+
 plt.savefig('/mnt/d/poisson.png')
 plt.show()
