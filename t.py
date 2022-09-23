44755312b3ca00f2eb55701f3e42bdbac9b3b03f
plusleung <42401487+plusleung@users.noreply.github.com>
2022-09-23 17:06:22 +0800

    Update t.py

--- t.py
+++ t.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import gamma
+from matplotlib import ticker
 
 def t(freedom):
     x = np.linspace(-10, 10, 1001)
@@ -12,6 +13,8 @@ def t(freedom):
 for freedom in [1, 2, 5]:
     x, y, s = t(freedom)
     plt.plot(x, y, label=r'$v=%d,\ \sigma=%.3f$' % (freedom, s))
+
+plt.gca().yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1,decimals=0))
 
 plt.legend()
 plt.savefig('/mnt/d/student_t.png')
